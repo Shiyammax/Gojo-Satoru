d59d0e15bdfdbb5b1e92c1ecd79cc13bf6c244b1
Shiyammax <108330773+Shiyammax@users.noreply.github.com>
2022-06-29 00:43:35 +0530

    Update setting.js
    Update

--- settings.js
+++ settings.js
@@ -19,8 +19,8 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
+global.owner = ['+94702410162']
+global.premium = ['+94702410162']
 global.ownernomer = 'Your Number'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
