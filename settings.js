336915e9c70884aaa0fef2110f7f3d9dce1d64cd
Shiyammax <108330773+Shiyammax@users.noreply.github.com>
2022-06-28 16:43:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94786830149']
+global.premium = ['94786830149']
+global.ownernomer = '94786830149'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.botname = 'shiyambot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
